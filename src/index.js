9ff0069e65a2dc2bef59548a702d10724cd5d9e4
Mark Muskardin <mark.muskardin@gmail.com>
2016-04-26 21:30:48 -0700

    removed call to injectTapEventPlugin

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react'
 import { AutoComplete } 		from 'material-ui';
 import JSONP 								from 'jsonp';
 import YoutubeFinder 				from 'youtube-finder';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
-injectTapEventPlugin();
 
 const googleAutoSuggestURL = '//suggestqueries.google.com/complete/search?client=youtube&ds=yt&q=';
 
