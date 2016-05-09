40efb50006f1de1b83ae84315dc2da09cabc1b02
Mark Muskardin <mark.muskardin@gmail.com>
2016-05-08 21:39:07 -0700

    indentation fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { AutoComplete } 		from 'material-ui';
-import JSONP 								from 'jsonp';
-import YoutubeFinder 				from 'youtube-finder';
+import { AutoComplete }     from 'material-ui';
+import JSONP                from 'jsonp';
+import YoutubeFinder        from 'youtube-finder';
 
 const googleAutoSuggestURL = '//suggestqueries.google.com/complete/search?client=youtube&ds=yt&q=';
 
@@ -19,25 +19,24 @@ class MaterialUIAutocomplete extends Component {
 
 	performSearch() {
 		var
-			self 	= this,
-			url 	= googleAutoSuggestURL + this.state.inputValue;
+		self 	= this,
+		url 	= googleAutoSuggestURL + this.state.inputValue;
 
 		if(this.state.inputValue !== '') {
-			JSONP(url, function(error, data){
-				var searchResults, retrievedSearchTerms;
+		JSONP(url, function(error, data){
+			var searchResults, retrievedSearchTerms;
 
-	      if(error) return console.log(error);
+			if(error) return console.log(error);
+			searchResults = data[1];
 
-				searchResults = data[1];
+			retrievedSearchTerms = searchResults.map(function(result) {
+				return result[0];
+			});
 
-        retrievedSearchTerms = searchResults.map(function(result) {
-        	return result[0];
-        });
-
-        self.setState({
-      		dataSource : retrievedSearchTerms
-      	});
-	    });
+			self.setState({
+				dataSource : retrievedSearchTerms
+				});
+			});
 		}
 	}
 
@@ -45,42 +44,42 @@ class MaterialUIAutocomplete extends Component {
 		var self = this;
 		this.setState({
 			inputValue : inputValue
-		},function(){
-			self.performSearch();
+			},function(){
+				self.performSearch();
 		});
 	}
 
 	onNewRequest(searchTerm) {
 		var self   = this;
-    var params = {
-      part        : 'id,snippet',
-      type        : 'video',
-      q           : this.state.inputValue,
-      maxResults  : this.props.maxResults <= 50 ? this.props.maxResults : '50'
-    }
+		var params = {
+			part        : 'id,snippet',
+			type        : 'video',
+			q           : this.state.inputValue,
+			maxResults  : this.props.maxResults <= 50 ? this.props.maxResults : '50'
+		}
 
-    this.YoutubeClient.search(params, function(error,results){
-      if(error) return console.log(error);
-      self.props.callback(results.items,searchTerm);
-      self.setState({
-      	dataSource : [],
-      	inputValue : ''
-      });
-    });
+		this.YoutubeClient.search(params, function(error,results) {
+			if(error) return console.log(error);
+			self.props.callback(results.items,searchTerm);
+			self.setState({
+				dataSource : [],
+				inputValue : ''
+			});
+		});
 	}
 
-  render() {
-    return <div>
-	    <AutoComplete
-	    	searchText 					={this.state.inputValue}
-	      floatingLabelText 	={this.props.placeHolder}
-	      filter 							={AutoComplete.noFilter}
-	      triggerUpdateOnFocus={true}
-	      dataSource 					={this.state.dataSource}
-	      onUpdateInput 			={this.onUpdateInput}
-	      onNewRequest 				={this.onNewRequest} />
-    </div>
-  }
+render() {
+	return <div>
+		<AutoComplete
+		searchText 					={this.state.inputValue}
+		floatingLabelText   ={this.props.placeHolder}
+		filter              ={AutoComplete.noFilter}
+		triggerUpdateOnFocus={true}
+		dataSource          ={this.state.dataSource}
+		onUpdateInput       ={this.onUpdateInput}
+		onNewRequest        ={this.onNewRequest} />
+		</div>
+	}
 }
 
 
