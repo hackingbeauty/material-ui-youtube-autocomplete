86b4aec3a67895d4e4bb429b140bc44ca2b6a19c
Mark Muskardin <mark.muskardin@gmail.com>
2016-05-08 21:43:06 -0700

    indentation tweaks

--- src/index.js
+++ src/index.js
@@ -6,81 +6,82 @@ import YoutubeFinder        from 'youtube-finder';
 const googleAutoSuggestURL = '//suggestqueries.google.com/complete/search?client=youtube&ds=yt&q=';
 
 class MaterialUIAutocomplete extends Component {
-	constructor(props) {
-		super(props);
-		this.onUpdateInput 		= this.onUpdateInput.bind(this);
-		this.onNewRequest 		= this.onNewRequest.bind(this);
-		this.YoutubeClient 		= YoutubeFinder.createClient({ key: this.props.apiKey });
-		this.state = {
-			dataSource : [],
-			inputValue : ''
-		}
-	}
+  constructor(props) {
+  super(props);
+  this.onUpdateInput  = this.onUpdateInput.bind(this);
+  this.onNewRequest   = this.onNewRequest.bind(this);
+  this.YoutubeClient  = YoutubeFinder.createClient({ key: this.props.apiKey });
+  this.state = {
+    dataSource : [],
+    inputValue : ''
+  }
+}
 
-	performSearch() {
-		var
-		self 	= this,
-		url 	= googleAutoSuggestURL + this.state.inputValue;
+performSearch() {
+  var
+    self = this,
+    url  = googleAutoSuggestURL + this.state.inputValue;
 
-		if(this.state.inputValue !== '') {
-			JSONP(url, function(error, data){
-				var searchResults, retrievedSearchTerms;
+  if(this.state.inputValue !== '') {
+    JSONP(url, function(error, data) {
+      var searchResults, retrievedSearchTerms;
 
-				if(error) return console.log(error);
+      if(error) return console.log(error);
 
-				searchResults = data[1];
+      searchResults = data[1];
 
-				retrievedSearchTerms = searchResults.map(function(result) {
-					return result[0];
-				});
+      retrievedSearchTerms = searchResults.map(function(result) {
+        return result[0];
+      });
 
-				self.setState({
-					dataSource : retrievedSearchTerms
-				});
-			});
-		}
-	}
+      self.setState({
+        dataSource : retrievedSearchTerms
+      });
+    });
+  }
+}
 
-	onUpdateInput(inputValue) {
-		var self = this;
-		this.setState({
-			inputValue : inputValue
-			},function(){
-				self.performSearch();
-		});
-	}
+onUpdateInput(inputValue) {
+  var self = this;
+  this.setState({
+    inputValue : inputValue
+    },function(){
+    self.performSearch();
+  });
+}
 
-	onNewRequest(searchTerm) {
-		var self   = this;
-		var params = {
-			part        : 'id,snippet',
-			type        : 'video',
-			q           : this.state.inputValue,
-			maxResults  : this.props.maxResults <= 50 ? this.props.maxResults : '50'
-		}
+onNewRequest(searchTerm) {
+var
+  self   = this,
+  params = {
+    part        : 'id,snippet',
+    type        : 'video',
+    q           : this.state.inputValue,
+    maxResults  : this.props.maxResults <= 50 ? this.props.maxResults : '50'
+  }
 
-		this.YoutubeClient.search(params, function(error,results) {
-			if(error) return console.log(error);
-			self.props.callback(results.items,searchTerm);
-			self.setState({
-				dataSource : [],
-				inputValue : ''
-			});
-		});
-	}
+  this.YoutubeClient.search(params, function(error,results) {
+    if(error) return console.log(error);
+    self.props.callback(results.items,searchTerm);
+    self.setState({
+      dataSource : [],
+      inputValue : ''
+    });
+  });
+}
 
-render() {
-	return <div>
-		<AutoComplete
-		searchText 					={this.state.inputValue}
-		floatingLabelText   ={this.props.placeHolder}
-		filter              ={AutoComplete.noFilter}
-		triggerUpdateOnFocus={true}
-		dataSource          ={this.state.dataSource}
-		onUpdateInput       ={this.onUpdateInput}
-		onNewRequest        ={this.onNewRequest} />
-		</div>
-	}
+  render() {
+    return <div>
+      <AutoComplete
+        searchText 					={this.state.inputValue}
+        floatingLabelText   ={this.props.placeHolder}
+        filter              ={AutoComplete.noFilter}
+        triggerUpdateOnFocus={true}
+        dataSource          ={this.state.dataSource}
+        onUpdateInput       ={this.onUpdateInput}
+        onNewRequest        ={this.onNewRequest} />
+      </div>
+  }
 }
 
 
