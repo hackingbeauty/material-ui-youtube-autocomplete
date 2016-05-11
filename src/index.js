855611a9200d58286ccb1a86deaaa1973cc55db4
Mark Muskardin <mark.muskardin@gmail.com>
2016-05-10 18:01:07 -0700

    fixed indentation

--- src/index.js
+++ src/index.js
@@ -7,68 +7,68 @@ const googleAutoSuggestURL = '//suggestqueries.google.com/complete/search?client
 
 class MaterialUIAutocomplete extends Component {
   constructor(props) {
-  super(props);
-  this.onUpdateInput  = this.onUpdateInput.bind(this);
-  this.onNewRequest   = this.onNewRequest.bind(this);
-  this.YoutubeClient  = YoutubeFinder.createClient({ key: this.props.apiKey });
-  this.state = {
-    dataSource : [],
-    inputValue : ''
-  }
-}
-
-performSearch() {
-  var
-    self = this,
-    url  = googleAutoSuggestURL + this.state.inputValue;
-
-  if(this.state.inputValue !== '') {
-    JSONP(url, function(error, data) {
-      var searchResults, retrievedSearchTerms;
-
-      if(error) return console.log(error);
-
-      searchResults = data[1];
-
-      retrievedSearchTerms = searchResults.map(function(result) {
-        return result[0];
-      });
-
-      self.setState({
-        dataSource : retrievedSearchTerms
-      });
-    });
-  }
-}
-
-onUpdateInput(inputValue) {
-  var self = this;
-  this.setState({
-    inputValue : inputValue
-    },function(){
-    self.performSearch();
-  });
-}
-
-onNewRequest(searchTerm) {
-var
-  self   = this,
-  params = {
-    part        : 'id,snippet',
-    type        : 'video',
-    q           : this.state.inputValue,
-    maxResults  : this.props.maxResults <= 50 ? this.props.maxResults : '50'
-  }
-
-  this.YoutubeClient.search(params, function(error,results) {
-    if(error) return console.log(error);
-    self.props.callback(results.items,searchTerm);
-    self.setState({
+    super(props);
+    this.onUpdateInput  = this.onUpdateInput.bind(this);
+    this.onNewRequest   = this.onNewRequest.bind(this);
+    this.YoutubeClient  = YoutubeFinder.createClient({ key: this.props.apiKey });
+    this.state = {
       dataSource : [],
       inputValue : ''
+    }
+  }
+
+  performSearch() {
+    var
+      self = this,
+      url  = googleAutoSuggestURL + this.state.inputValue;
+
+    if(this.state.inputValue !== '') {
+      JSONP(url, function(error, data) {
+        var searchResults, retrievedSearchTerms;
+
+        if(error) return console.log(error);
+
+        searchResults = data[1];
+
+        retrievedSearchTerms = searchResults.map(function(result) {
+          return result[0];
+        });
+
+        self.setState({
+          dataSource : retrievedSearchTerms
+        });
+      });
+    }
+  }
+
+  onUpdateInput(inputValue) {
+    var self = this;
+    this.setState({
+      inputValue : inputValue
+      },function(){
+      self.performSearch();
     });
-  });
-}
+  }
+
+  onNewRequest(searchTerm) {
+    var
+      self   = this,
+      params = {
+        part        : 'id,snippet',
+        type        : 'video',
+        q           : this.state.inputValue,
+        maxResults  : this.props.maxResults <= 50 ? this.props.maxResults : '50'
+      }
+
+    this.YoutubeClient.search(params, function(error,results) {
+      if(error) return console.log(error);
+      self.props.callback(results.items,searchTerm);
+      self.setState({
+        dataSource : [],
+        inputValue : ''
+      });
+    });
+  }
 
   render() {
     return <div>
