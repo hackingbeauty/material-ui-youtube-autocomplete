593d92b1aea20e06e02ad9209341e8510f7ee08b
Mark Muskardin <mark.muskardin@gmail.com>
2016-06-10 12:37:28 -0700

    updated blog post

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { AutoComplete }     from 'material-ui';
 import JSONP                from 'jsonp';
-import YoutubeFinder        from 'youtube-finder';
+import getMuiTheme          from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider     from 'material-ui/styles/MuiThemeProvider';
 
 const googleAutoSuggestURL = '//suggestqueries.google.com/complete/search?client=youtube&ds=yt&q=';
 
@@ -9,35 +10,9 @@ class MaterialUIAutocomplete extends Component {
   constructor(props) {
     super(props);
     this.onUpdateInput  = this.onUpdateInput.bind(this);
-    this.onNewRequest   = this.onNewRequest.bind(this);
-    this.YoutubeClient  = YoutubeFinder.createClient({ key: this.props.apiKey });
     this.state = {
       dataSource : [],
       inputValue : ''
-    }
-  }
-
-  performSearch() {
-    var
-      self = this,
-      url  = googleAutoSuggestURL + this.state.inputValue;
-
-    if(this.state.inputValue !== '') {
-      JSONP(url, function(error, data) {
-        var searchResults, retrievedSearchTerms;
-
-        if(error) return console.log(error);
-
-        searchResults = data[1];
-
-        retrievedSearchTerms = searchResults.map(function(result) {
-          return result[0];
-        });
-
-        self.setState({
-          dataSource : retrievedSearchTerms
-        });
-      });
     }
   }
 
@@ -50,37 +25,24 @@ class MaterialUIAutocomplete extends Component {
     });
   }
 
-  onNewRequest(searchTerm) {
+  performSearch() {
     var
-      self   = this,
-      params = {
-        part        : 'id,snippet',
-        type        : 'video',
-        q           : this.state.inputValue,
-        maxResults  : this.props.maxResults <= 50 ? this.props.maxResults : '50'
-      }
+      self = this,
+      url  = googleAutoSuggestURL + this.state.inputValue;
 
-    this.YoutubeClient.search(params, function(error,results) {
-      if(error) return console.log(error);
-      self.props.callback(results.items,searchTerm);
-      self.setState({
-        dataSource : [],
-        inputValue : ''
+    if(this.state.inputValue !== '') {
+      JSONP(url, function(error, data) {
+        //Will maninuplate video search results here
       });
-    });
+    }
   }
 
   render() {
-    return <div>
+    return <MuiThemeProvider muiTheme={getMuiTheme()}>
       <AutoComplete
-        searchText          ={this.state.inputValue}
-        floatingLabelText   ={this.props.placeHolder}
-        filter              ={AutoComplete.noFilter}
-        triggerUpdateOnFocus={true}
-        dataSource          ={this.state.dataSource}
-        onUpdateInput       ={this.onUpdateInput}
-        onNewRequest        ={this.onNewRequest} />
-      </div>
+        dataSource    ={this.state.dataSource}
+        onUpdateInput ={this.onUpdateInput} />
+      </MuiThemeProvider>
   }
 }
 
