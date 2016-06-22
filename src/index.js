7c9e0d5c2f001b144f0f2ae9dc255f07a21a77a4
Mark Muskardin <mark.muskardin@gmail.com>
2016-06-22 11:59:42 -0700

    more work

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { AutoComplete }     from 'material-ui';
 import JSONP                from 'jsonp';
 import getMuiTheme          from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider     from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 const googleAutoSuggestURL = '//suggestqueries.google.com/complete/search?client=youtube&ds=yt&q=';
 
