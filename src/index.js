41e5fa1523a8b87ed6e7dd5e0ffda6da07cec0e5
Mark Muskardin <mark.muskardin@gmail.com>
2016-06-22 11:57:54 -0700

    Fixes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import { AutoComplete }     from 'material-ui';
 import JSONP                from 'jsonp';
 import YoutubeFinder        from 'youtube-finder';
+import getMuiTheme          from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider     from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 const googleAutoSuggestURL = '//suggestqueries.google.com/complete/search?client=youtube&ds=yt&q=';
 
@@ -71,16 +76,16 @@ class MaterialUIAutocomplete extends Component {
   }
 
   render() {
-    return <div>
+    return <MuiThemeProvider muiTheme={getMuiTheme()}>
       <AutoComplete
         searchText          ={this.state.inputValue}
         floatingLabelText   ={this.props.placeHolder}
         filter              ={AutoComplete.noFilter}
-        triggerUpdateOnFocus={true}
+        openOnFocus         ={true}
         dataSource          ={this.state.dataSource}
         onUpdateInput       ={this.onUpdateInput}
         onNewRequest        ={this.onNewRequest} />
-      </div>
+    </MuiThemeProvider>
   }
 }
 
